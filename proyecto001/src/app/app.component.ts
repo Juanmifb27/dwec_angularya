3f510cbea050647df72d99e12ad841486f43a3fc
juanmifb27-admin <juanmanuel.ferrera-berlanga@iesruizgijon.com>
2025-02-14 21:15:30 +0100

    Actualizado hasta AngularYa 6

--- proyecto001/src/app/app.component.ts
+++ proyecto001/src/app/app.component.ts
@@ -7,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'proyecto001';
+  title : string = 'proyecto001';
+  nombre = 'Juanmi';
+  edad : number = 12;
+  fumador = false;
+  sueldos : number[] = [1200, 1500, 1700];
+
+  contador = 0;
+
+  provincias = [
+    {id:1, nombre:"Huelva"},
+    {id:2, nombre:"Sevilla"},
+    {id:3, nombre:"Almeria"},
+    {id:4, nombre:"Cordoba"},
+    {id:5, nombre:"Cadiz"},
+  ];
+
+  esFumador(){
+    return this.fumador?"Es Fumador":"No es Fumador";
+  }
+  diHola(){
+    alert("Hola!");
+  }
+  incrementar(){
+    this.contador++;
+  }
+  decrementar(){
+    this.contador--;
+  }
+
+
 }
